a7760881ec57030dd7ccfa32658e3445bd49c25b
Donjay Space Digitals <97373532+Donjayspace@users.noreply.github.com>
2023-03-01 18:48:25 +0100

    Update script.js

--- search/script.js
+++ search/script.js
@@ -1,3 +1,21 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+
+
 query.
 		 * @param {string} sValue - Value to be validated.
 		 */
